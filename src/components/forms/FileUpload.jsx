fe3b4857ee7cc2b904253b9a0c4cda485238919a
Mr-Theppakorn <38773611+Mr-Theppakorn@users.noreply.github.com>
2024-04-05 14:38:08 +0700

    Update FileUpload.jsx

--- src/components/forms/FileUpload.jsx
+++ src/components/forms/FileUpload.jsx
@@ -1,40 +1,37 @@
 import React, { useRef } from 'react'
 import Button from '@mui/material/Button'
-import Resizer from 'react-image-file-resizer'
 import { useSelector } from 'react-redux'
 import { removeImages, uploadImages } from '../../api/product'
 import Badge from '@mui/material/Badge';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { covertBase64 } from '../../pages/user/MyProfile'
 
 const FileUpload = ({ values, setValues, setLoading }) => {
     const { token } = useSelector(state => state.user);
     const fileInputRef = useRef(null);
-    console.log(values);
+    console.log(values, 'values');
 
     const fileUpload = async (e) => {
         setLoading(true);
         let files = e.target.files;
         let allUploadedFiles = [];
-
         if (files) {
             try {
                 const uploadPromises = Array.from(files).map(async (file) => {
-                    return new Promise((resolve, reject) => {
-                        Resizer.imageFileResizer(file, 720, 720, 'JPEG', 100, 0, async (uri) => {
-                            try {
-                                const response = await uploadImages(token, uri);
-                                allUploadedFiles.push(response.data);
-                                resolve();
-                            } catch (error) {
-                                reject(error);
-                            }
-                        }, 'base64');
+                    return new Promise(async (resolve, reject) => {
+                        try {
+                            const base64 = await covertBase64(file);
+                            const response = await uploadImages(token, base64);
+                            allUploadedFiles.push(response.data);
+                            resolve();
+                        } catch (error) {
+                            reject(error);
+                        }
                     });
                 });
-
                 await Promise.all(uploadPromises);
-                setValues({ ...values, images: allUploadedFiles });
+                setValues({ ...values, images: [...values.images, ...allUploadedFiles] });
             } catch (error) {
                 console.error('Error uploading files:', error);
             } finally {
@@ -43,40 +40,15 @@ const FileUpload = ({ values, setValues, setLoading }) => {
         }
     };
 
-
-
-    const imageResizeEx = (e) => {
-        let files = e.target.files;
-        if (files) {
-            let newImages = [];
-            for (let i = 0; i < files.length; i++) {
-                Resizer.imageFileResizer(
-                    files[i],
-                    720,
-                    720,
-                    'JPEG',
-                    100,
-                    0,
-                    (uri) => {
-                        newImages.push(uri);
-                        if (newImages.length === files.length) {
-                            setImages(newImages);
-                        }
-                    },
-                    'base64'
-                );
-            }
-        }
-    };
-
     const onRemoveImage = (public_id) => {
         setLoading(true);
-        removeImages(token, public_id)
+        removeImages(token, public_id, values._id)
             .then((res) => {
-                const filteredImage = images.filter((i) => {
+                console.log(res.data, 'res.data');
+                const filteredImage = values.images.filter((i) => {
                     return i.public_id !== public_id;
                 });
-                setImages(filteredImage);
+                setValues({ ...values, images: filteredImage });
                 setLoading(false);
             }).catch((err) => {
                 console.log(err);
